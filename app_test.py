079d93eb53bb9a334b79f060916ab9e307712158
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-11-06 09:26:09 +0000

    unit test

--- app_test.py
+++ app_test.py
@@ -10,7 +10,7 @@ class CitiesTestCase(unittest.TestCase):
     response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.data, json.dumps(['Amsterdam', 'San Francisco',
-      'Berlin','New York']))
+      'Berlin','London']))
 
 if __name__ == '__main__':
     unittest.main()
